97a710ce806d4709356633323d1bea9f2a862cdb
Thomas Michalski <thomas_michalski@hotmail.com>
2025-04-08 12:20:49 -0400

    removed unwanted files

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error('Missing REACT_APP_API_BASE_URL');
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // change if needed
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL,
 });
 
 export default api;
